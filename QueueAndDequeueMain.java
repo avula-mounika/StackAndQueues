7113496df8316037fe3852d4add9cb37e3496a01
avula-mounika <goudmounika13@gmail.com>
2021-09-14 18:52:38 +0530

    queue and dequeue

---
+++ QueueAndDequeueMain.java
@@ -0,0 +1,28 @@
+package com.bl.stack;
+
+public class QueueAndDequeueMain {
+
+	public static void main(String[] args) {
+		// create a queue of capacity 5
+		Queue q = new Queue(5);
+
+		q.enqueue(56);
+		q.enqueue(30);
+		q.enqueue(70);
+
+		System.out.println("The front element is " + q.peek());
+		q.dequeue();
+		System.out.println("The front element is " + q.peek());
+
+		System.out.println("The queue size is " + q.size());
+
+		q.dequeue();
+		q.dequeue();
+
+		if (q.isEmpty()) {
+			System.out.println("The queue is empty");
+		} else {
+			System.out.println("The queue is not empty");
+		}
+	}
+}
